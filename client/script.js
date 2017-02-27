6de860ccb4a1aef21f7642cbcc2f7aca4d3b3961
Ömer Faruk Kırlı <omerfarukkirli1994@gmail.com>
2017-02-28 00:17:28 +0300

    added difie hellman thing

--- client/script.js
+++ client/script.js
@@ -1,22 +1,32 @@
 
-var inputName,inputMessage,messageBox;
-var socket = io.connect('https://kriptolojiassignment1.herokuapp.com/'); //https://kriptolojiassignment1.herokuapp.com/
+var inputName,inputMessage,messageBox,inputKey,inputPrivateKey,labelPublicKey;
+//var socket = io.connect('localhost:3000');
+var socket = io.connect('https://kriptolojiassignment1.herokuapp.com/');
 
 inputName = document.getElementById('inputName');
 inputMessage = document.getElementById('inputMessage');
 messageBox = document.getElementById('messageBox');
 inputKey = document.getElementById('inputKey');
+labelPublicKey = document.getElementById('labelPublicKey');
+inputPrivateKey = document.getElementById('inputPrivateKey');
 
+var p2 = 11;
+var alfa = 2;
+var myPublicKey = -1;
+var otherPublicKey = -1;
+var privateKey = -1;
+var commonKey = -1;
+var key = -1;
 inputMessage.addEventListener("keyup", function(event) {
-    event.preventDefault();
-    if (event.keyCode == 13) {
-        document.getElementById("sendButton").click();
-    }
+event.preventDefault();
+if (event.keyCode == 13) {
+    document.getElementById("sendButton").click();
+}
 });
 
 function sendMessage()
 {
-socket.emit('sendMessage',{name:inputName.value, message: cipherText(inputMessage.value)});
+	socket.emit('sendMessage',{name:inputName.value, message: cipherText(inputMessage.value)});
 
 	messageBox.value += inputName.value + " : " + inputMessage.value + " /|\\ (C):" + cipherText(inputMessage.value) + "\n";
 	inputMessage.value = "";
@@ -32,7 +42,7 @@ function cipherText(text)
 	var count = Math.floor(text.length / 6 );
 	p = [2,4,0,5,3,1];
 	var c = 1;
-	var key = parseInt(inputKey.value) % 26;
+	key = parseInt(inputKey.value) % 26;
 	result = "";
 	while(count > 0)
 	{
@@ -56,17 +66,21 @@ function cipherText(text)
 		count--;
 	}
 
+	if(text.length % 6 > 0)
+		result += text.substring( (6*c) - 6 , 6*c );
 
-  return result;
-}
+	messageBox.scrollTop = messageBox.scrollHeight;
+	return result;
+	}
 
-socket.on('cipherMessage',function (data){
+	socket.on('cipherMessage',function (data){
 
 	if(inputName.value  == data.name)
 		return;
-	
+
 	messageBox.value += data.name + " : " + decipherText(data.message) +"\n";
-	
+
+
 });
 
 function decipherText(text)
@@ -78,7 +92,7 @@ function decipherText(text)
 	var count = Math.floor(text.length / 6 );
 	p = [2,5,0,4,1,3];
 	var c = 1;
-	var key = parseInt(inputKey.value) % 26;
+	key = parseInt(inputKey.value) % 26;
 	result = "";
 
 	while(count > 0)
@@ -98,15 +112,77 @@ function decipherText(text)
 		c++;
 		count--;
 	}
+
+	if(text.length % 6 > 0)
+		result += text.substring( (6*c) - 6 , 6*c );
+	messageBox.scrollTop = messageBox.scrollHeight;
 	return result;
 }
 
-function mod(n, m) {
-        return ((n % m) + m) % m;
+function mod(n, m)
+{
+    return ((n % m) + m) % m;
 }
 
 
-  socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data2' });
-  });
+socket.on('news', function (data) {
+	console.log(data);
+	socket.emit('my other event', { my: 'data2' });
+});
+
+
+function generatePublicKey()
+{
+	privateKey = parseInt(inputPrivateKey.value);
+
+	if(0 <= privateKey && privateKey <= p2-2)
+	{
+	
+		myPublicKey = Math.pow(alfa,privateKey) % p2;
+		labelPublicKey.textContent = "Your Public Key Is : " + myPublicKey;
+
+	}
+	else
+	{
+		labelPublicKey.textContent = "Your private key must be between 0 and " + (p2-2)  + "  !";
+		return;
+	}
+
+
+}
+
+function calculateCommonKey() 
+{
+	otherPublicKey = parseInt(document.getElementById('inputPublicKey').value);
+
+	commonKey = Math.pow(otherPublicKey,privateKey) % p2;
+
+	inputKey.value = commonKey;
+
+}
+
+function sendPublicKey() {
+	if(myPublicKey < 0)
+	{
+		labelPublicKey.textContent = "First Create Public Key Here !";
+	}
+	else
+	{
+		socket.emit('sendMessage',{name:inputName.value, message: "My Public key is " + myPublicKey});
+		document.getElementById('inputKey').value ="";
+	messageBox.value += inputName.value + " : My Public key is " + myPublicKey + "\n";
+		labelPublicKey.textContent = "Public key : " + myPublicKey + " shared!";
+	}
+}
+
+function power(a, b, prime) {
+	if (b <= 0) {
+	    return 1;
+	} else if (b === 1) {
+	    return a % prime;
+	} else if (b % 2 === 0) {
+	    return power(a * a, b / 2 | 0, prime) % prime;
+	} else {
+	    return power(a * a, b / 2 | 0, prime) * a % prime;
+	}
+}
